341f2d664e27109726e9165435a05393174fab29
ardakavas <kavasarda@hotmail.com>
2022-10-03 17:29:55 +0300

    edited.

--- src/main/java/com/petclinic/repository/OwnerRepository.java
+++ src/main/java/com/petclinic/repository/OwnerRepository.java
@@ -3,5 +3,5 @@ package com.petclinic.repository;
 import com.petclinic.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OwnerRepository  extends JpaRepository<Owner,Long> {
+public interface OwnerRepository  extends JpaRepository<Owner,Integer> {
 }
