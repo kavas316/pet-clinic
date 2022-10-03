61348048f159b1258c5c4d74a39e2f36aa91c4c4
ardakavas <kavasarda@hotmail.com>
2022-10-03 17:10:55 +0300

    Owner Repository added.

---
+++ src/main/java/com/petclinic/repository/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.petclinic.repository;
+
+import com.petclinic.model.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository  extends JpaRepository<Owner,Long> {
+}
