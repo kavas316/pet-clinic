7ec9d150b6ea0ba65bab0d39ae084a7b75412994
ardakavas <kavasarda@hotmail.com>
2022-10-18 04:06:57 +0300

    Search box configuration added. (Search by pet Name or Owner name-surname.)

--- src/main/java/com/petclinic/repository/PetsRepository.java
+++ src/main/java/com/petclinic/repository/PetsRepository.java
@@ -1,6 +1,7 @@
 package com.petclinic.repository;
 
 import com.petclinic.model.Pets;
+import com.petclinic.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,8 @@ import java.util.List;
 @Repository
 public interface PetsRepository extends JpaRepository<Pets,Integer>{
 
-    @Query(value="SELECT * FROM Pets p WHERE p.name like %:keyword%",nativeQuery = true)
+    //@Query(value="SELECT * FROM Pets p WHERE p.name OR p.owner.name like %:keyword%",nativeQuery = true)
+    @Query(value="SELECT * FROM Pets p INNER JOIN Owners ON p.owner_id = Owners.id WHERE Owners.name  like %:keyword% OR p.name like %:keyword% OR Owners.surname like %:keyword%",nativeQuery = true)
     List<Pets> findByKeyword(@Param("keyword") String keyword);
 }
 
